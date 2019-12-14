0533de68fb62c8398edea538f3628503a57b0093
Ricky <53333122+rickyexe@users.noreply.github.com>
2019-12-14 21:59:47 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,81 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,300&display=swap" rel="stylesheet">
+
+    <style type="text/css">
+      
+      @media only screen and (max-width: 600px) {
+              #things1 {
+                font-size: 35pt;
+                margin-left: -10%;
+              }
+
+              #things2 {
+                margin-left: -10%;
+              }
+      }
+
+
+    </style>
+
+    <title>Home</title>
+  </head>
+<body style="font-family: 'Montserrat', sans-serif;">
+
+
+<ul class="nav bg-light nav-pills nav-fill justify-content-center">
+  <li class="nav-item">
+    <a class="nav-link active" href="index.php">Home</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="profile.php">Profile</a>
+  </li>
+   <li class="nav-item">
+    <a class="nav-link" href="portfolio.php">Portfolio</a>
+  </li>
+
+</ul>
+
+<div class="container-fluid">
+<div class="row">
+    <div class="col-md-7">
+      <div class="media-body ml-5">
+      <h3 class="display-3" id="things2" style="margin-top: 25%;" > I'm <span style="color: #7D2941">Ricky</span></h3>
+    </div>
+    <div class="media-body ml-5">
+      <h3 class="display-3" id="things1" style=" font-weight: 200;" >A Programmer from Semarang</h3>
+    </div>
+    <div class="media-body" style="margin-top: 35%; text-align: center;">
+      <h3 class="display-5" style=" font-weight: 400;" >Kindly Check My <a href="profile.php" style="text-decoration: none;font-weight: 400;">Profile</a> For More Info</h3>
+    </div>
+
+
+
+    </div>
+    <div class="col-md-5">
+      <div >
+      <img  src="rickygideon.jpg" class="img-fluid">  
+      </div>
+      
+    </div>
+  </div>
+</div>
+</div>
+
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
+
+
---
+++ portfolio.php
@@ -0,0 +1,163 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+     <link href="https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,300&display=swap" rel="stylesheet"> 
+
+
+     </style>
+
+
+    <title>Profile</title>
+  </head>
+  <body style="font-family: 'Montserrat', sans-serif;">
+
+
+
+<ul class="nav nav-pills nav-fill justify-content-center">
+  <li class="nav-item">
+    <a class="nav-link" href="index.php">Home</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="profile.php">Profile</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link active" href="portfolio.php">Portfolio</a>
+  </li>
+</ul>
+
+<div class="row">
+  <h1 class="display-4 mx-auto mt-3">PORTFOLIO</h1>
+</div>
+<div class="row">
+  <div class="col-md-6">
+    <h1 class="ml-5 mt-3">Money Management System</h1>
+    <p class="ml-5">A website that built using Laravel Framework, Bootstrap, and also MySQL.
+      Website Features : <br>
+      1. In this website you can note your income and expense. <br>
+      2. Report for income and expense ratio, income trend, and also expense trend.<br>
+      3. Make a savings plan for your goals.<br>
+      4. Create your own transaction category
+  </p>
+  </div>
+  <div class="col-md-6">
+    <img src="moneymanagement.png" class="img-fluid">
+    
+  </div>
+</div>
+<div class="row">
+ 
+  <div class="col-md-6 mt-3">
+        <img src="jai.png" class="img-fluid mt-3">
+    
+  </div>
+   <div class="col-md-6 mt-3">
+     <h1 class="ml-3 mt-3">Jatim Autocomp Indonesia Inventory System</h1>
+    <p class="ml-3">A website that built using Laravel Framework, Bootstrap, and also SQL Server as the DBMS.
+      It's a team project and my part is to make the report module such as : <br>
+      1. Pemasukan Barang<br>
+      2. Pengeluaran Barang<br>
+      3. Mutasi Bahan Baku<br>
+      4. Mutasi Barang Jadi<br>
+      5. Mutasi Alat Kantor<br>
+      6. Mutasi Scrap
+  </p>
+  </div>
+</div>
+
+<div class="row">
+  <div class="col-md-12">
+    <h1 class="mt-3 text-center">Professor Flashlight</h1>
+    <p class="mt-3 text-center">A game about zombie shooter made with c# made for Student Challenge Ubaya 2017 <br>
+            It's a 2D game where you can shoot zombies and save some civillian <br>
+          Features :<br>
+    1.High Score<br>
+    2.Survival Mode<br>
+    3.Bomb<br>
+    4.Mission Mode
+
+  </p>
+  </div>
+  
+</div>
+
+<div class="row">
+  <div class="col-md-6">
+    <img src="menuflash.jpg" class="img-fluid mt-3 ml-1">
+    
+  </div>
+  <div class="col-md-6">
+    <img src="gamefl.jpg" class="img-fluid mt-3 mr-1">
+  </div>
+</div>
+
+
+
+
+
+
+
+<div class="row">
+    <div class="col-md-12 mt-3">
+     <h1 class="mt-3 text-center">Sehatin</h1>
+    <p class="text-center">An Android apps written in Kotlin to see restaurant list in Surabaya and give review to it <br>
+      Features : <br>
+      1. Add and See Review<br>
+      2. Open Maps<br>
+      3. See News About Healthy Life
+  </p>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+<div class="row">
+
+  <div class="col-md-12">
+    <h1 class="mt-3 text-center">Instagram Prototype Backend</h1>
+    <p class="text-center">An Instagram prototype made for fun using native PHP <br>
+      It has basic functionality of Instagram such as : <br>
+      1. Register and Login. <br>
+      2. Upload Multiple Photos.<br>
+      3.Comment.<br>
+      4. Like.<br>
+      5. User Search<br>
+  </p>
+  </div>
+  
+  
+</div>
+
+<div class="row">
+  <div class="col-md-12">
+    <h1 class="display-4 text-center">You can check some of the project on my <a href="https://github.com/rickyexe">Github</a> repository</h1>
+  </div>
+</div>
+
+
+
+
+
+
+</div>
+ 
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+  <footer>
+    
+  </footer>
+</html>
